8b09c0c4d478362cf2253a682e2519436e3a24de
Jeongmin Woo <greenblues1190@gmail.com>
2020-12-13 02:44:54 +0900

    Added components on LandingPage

---
+++ client/src/components/config/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+// The MovieDB API URL prefix
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
